74d62dfbd28f11e8a560e9ef5475b91e444923a6
Alejandro Galue <agalue@opennms.org>
2015-05-28 10:50:43 -0400

    Initial working version of Laser Defender 2D

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Energy : MonoBehaviour {
+
+	public float energyValue = 50;
+	public float speed = 5f;
+
+	void Update () {
+		transform.Rotate(new Vector3(0,0,45) * Time.deltaTime * 5f);
+	}
+	
+	public void Throw ()
+	{
+		rigidbody2D.velocity = Vector3.down * speed;
+	}
+	
+	public float GetHealth ()
+	{
+		return energyValue;
+	}
+	
+	public void Consume ()
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float health = 1000;
+	public float speed = 15.0f;
+	public float padding = 0.5f;
+	public float fireRate = 0.5f;
+	
+	public AudioClip fireSound;
+	public float fireSoundVolume = 1f;
+	public AudioClip deathSound;
+	public float deathSoundVolume = 1f;
+	public GameObject projectilePrefab;
+	public GameObject explosionPrefab;
+	
+	private float xMin, xMax;
+	private float nextFire = 0.0f;
+	private float healthRemaining;
+	private LevelManager levelManager;
+	private Slider healthSlider;
+	
+	void Awake ()
+	{
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+		healthSlider = GameObject.FindObjectOfType<Slider> (); // There is only one slider object.
+		healthRemaining = health;
+		healthSlider.maxValue = health;
+		healthSlider.value = health;
+	}
+
+	void Start ()
+	{
+		// Setting limits on X Axis based on the Camera's Viewport.
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distanceToCamera));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distanceToCamera));
+		xMin = leftBoundary.x + padding;
+		xMax = rightBoundary.x - padding;
+	}
+
+	void Update ()
+	{
+		// Move the player
+		float moveHorizontal = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		transform.Translate (new Vector3 (moveHorizontal, 0, 0));
+
+		// Restrict the player's movement to the Viewport width.
+		Vector3 pos = transform.position;
+		pos.x = Mathf.Clamp (pos.x, xMin, xMax);
+		transform.position = pos;
+		
+		// Fire
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Fire ();
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("EnemyProjectile")) {
+			Projectile missile = other.gameObject.GetComponent<Projectile> ();
+			healthRemaining -= missile.GetDamage ();
+			healthSlider.value = healthRemaining;
+			missile.Hit ();
+			if (healthRemaining <= 0) {
+				Die ();
+			}
+		}
+		if (other.gameObject.CompareTag ("Energy")) {
+			if (healthRemaining < health) { // If the player has maximum health, picking energy doesn't make sense.
+				Energy energy = other.gameObject.GetComponent<Energy> ();
+				healthRemaining += energy.GetHealth ();
+				healthSlider.value = healthRemaining;
+				energy.Consume ();
+			}
+		}
+	}
+		
+	void Fire ()
+	{
+		GameObject projectile = Instantiate (projectilePrefab, transform.position, Quaternion.identity) as GameObject;
+		projectile.GetComponent<Projectile> ().Launch (Vector3.up);
+		AudioSource.PlayClipAtPoint (fireSound, transform.position, fireSoundVolume);
+	}
+	
+	void Die ()
+	{
+		AudioSource.PlayClipAtPoint (deathSound, transform.position, deathSoundVolume);
+		Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+		levelManager.GameOver ();
+	}
+}
